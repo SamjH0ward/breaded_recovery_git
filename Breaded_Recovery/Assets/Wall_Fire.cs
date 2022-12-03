cc89875d60e86b2674006984af99ef601b4881cd
Haedhaze <119425079+Haedhaze@users.noreply.github.com>
2022-12-03 16:27:50 +0000

    Complete wall of fire
    Barring graphics, wall of fire should be complete

---
+++ Breaded_Recovery/Assets/Wall_Fire.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Fire : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private BoxCollider2D Collision;
+    [SerializeField] private float speed = 4;
+    [SerializeField] private int dmg = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rigid = GetComponent<Rigidbody2D>();
+        Collision = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = -transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IDamageable damageable = other.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.TakeDamage(dmg);
+        }
+        Destroy(Collision);
+    }
+}
---
+++ Breaded_Recovery/Assets/wallFireSpawn.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallFireSpawn : MonoBehaviour
+{
+    private GameObject Fire1;
+    private GameObject Fire2;
+    private GameObject Fire3;
+    private GameObject Fire4;
+    private GameObject Fire5;
+    private GameObject Fire6;
+    private GameObject Fire7;
+    private GameObject Fire8;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fire1 = GameObject.Find("Fire1");
+        Fire2 = GameObject.Find("Fire2");
+        Fire3 = GameObject.Find("Fire3");
+        Fire4 = GameObject.Find("Fire4");
+        Fire5 = GameObject.Find("Fire5");
+        Fire6 = GameObject.Find("Fire6");
+        Fire7 = GameObject.Find("Fire7");
+        Fire8 = GameObject.Find("Fire8");
+        
+
+        int gap = Random.Range(1, 9);
+
+        switch (gap)
+        {
+            case 1:
+                Destroy(Fire1);
+                break;
+            case 2:
+                Destroy(Fire2);
+                break;
+            case 3:
+                Destroy(Fire3);
+                break;
+            case 4:
+                Destroy(Fire4);
+                break;
+            case 5:
+                Destroy(Fire5);
+                break;
+            case 6:
+                Destroy(Fire6);
+                break;
+            case 7:
+                Destroy(Fire7);
+                break;
+            case 8:
+                Destroy(Fire8);
+                break;
+            default:
+                Destroy(gameObject);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
