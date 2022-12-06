f9b7fc5a6d7fcdb3daf0bf8f3bf0a633ecafec9a
ItsDooc <119859973+ItsDooc@users.noreply.github.com>
2022-12-06 15:18:46 +0000

    Scrolling Background

---
+++ Breaded_Recovery/Assets/Scripts/GameSystems/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    public float ScrollingSpeed = 0.5f;
+    private Renderer renderer;
+    private Vector2 savedOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        savedOffset = new Vector2(0, Time.time*ScrollingSpeed);
+        renderer.material.mainTextureOffset = savedOffset;
+    }
+}
