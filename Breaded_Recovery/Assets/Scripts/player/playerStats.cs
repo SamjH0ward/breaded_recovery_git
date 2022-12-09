7219a6581e422611fa86cdba3b7a5e83a6436040
Sam <samjhoward2003@gmail.com>
2022-12-09 09:44:17 +0000

    finalized main menu & tweaked some ship stats

--- Breaded_Recovery/Assets/Scripts/player/playerStats.cs
+++ Breaded_Recovery/Assets/Scripts/player/playerStats.cs
@@ -21,35 +21,35 @@ public class playerStats : MonoBehaviour
         switch(shipType){
             case "cruiser":
                 maxHealth = 100;
-                baseDamage = 5;
+                baseDamage = 7;
                 playerSpeed = 5;
                 armor = 8;
                 dodgeChance = 3;
                 break;
             case "fighter":
                 maxHealth = 90;
-                baseDamage = 8;
+                baseDamage = 9;
                 playerSpeed = 6;
                 armor = 8;
                 dodgeChance = 3;
                 break;
             case "racer":
                 maxHealth = 80;
-                baseDamage = 4;
+                baseDamage = 5;
                 playerSpeed = 7;
                 armor = 5;
                 dodgeChance = 8;
                 break;
             case "behemoth":
                 maxHealth = 120;
-                baseDamage = 4;
+                baseDamage = 6;
                 playerSpeed = 3;
                 armor = 12;
                 dodgeChance = 0;
                 break;
             case "destroyer":
                 maxHealth = 110;
-                baseDamage = 8;
+                baseDamage = 9;
                 playerSpeed = 3;
                 armor = 10;
                 dodgeChance = 0;
